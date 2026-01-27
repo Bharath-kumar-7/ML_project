5a9727aff16c8c91013351ec0d293f915645d44a
bharath <badagalabharath123@gmail.com>
2026-01-27 20:49:35 +0530

    add requirement

--- list_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyB5H6b-gUMM85dLzPatSEp004EDlyQPA74")
-
-models = genai.list_models()
-
-for model in models:
-    print(model.name, "->", model.supported_generation_methods)
